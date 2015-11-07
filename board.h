c6ff498b253f322cdba730e9f993e8843f32d29d
Axl Cruz-Garcia <axl.cruz8344@gmail.com>
2015-11-07 11:43:44 -0500

    Changed the i/o. Final commit 11/7

--- board.h
+++ board.h
@@ -1,6 +1,6 @@
-#include <iostream>;
+#include <iostream>
 using namespace std;
-#include <cstring>;
+#include <cstring>
 #ifndef TEST_H
 #define	TEST_H
 
@@ -9,32 +9,70 @@ using namespace std;
 class Board
 {
  public:
-    string x1="1";
-    string x2="2";
-    string x3="3";
-    string x4="4";
-    string x5="5";
-    string x6="6";
-    string x7="7";
-    string x8="8";
-    string x9="9";
+    string x1;
+    string x2;
+    string x3;
+    string x4;
+    string x5;
+    string x6;
+    string x7;
+    string x8;
+    string x9;
      
     
     //Member Functions
-    
+    void setString(string x, string val);
     string printBoard(void);
     
 };
 
-    string Board::printBoard(void)
+void Board::setString(string x, string val){
+    if(x=="x1"){
+        x1=val;
+    }
+     if(x=="x2"){
+        x2=val;
+    }
+     if(x=="x3"){
+        x3=val;
+    }
+     if(x=="x4"){
+        x4=val;
+    }
+     if(x=="x5"){
+        x5=val;
+    }
+     if(x=="x6"){
+        x6=val;
+    }
+     if(x=="x7"){
+        x7=val;
+    }
+     if(x=="x8"){
+        x8=val;
+    }
+     if(x=="x9"){
+        x9=val;
+    }
+    
+}
+
+string Board::printBoard(void)
 {
         return x1 + "|" + x2 + "|" + x3+"\n"
-              +"_"+" "+"_"+" "+"_"+" "+"\n"
+             +"_"+" "+"_"+" "+"_"+" "+"\n" 
               +x4 + "|" + x5 + "|" + x6+"\n"
               +"_"+" "+"_"+" "+"_"+" "+"\n"
               +x7 + "|" + x8 + "|" + x9+"\n";
          
 
+}
+
+void checkInput(string input,Board board){
+    if(input=="1"){
+        board.setString("x1","X");
+    }
+    
 }
 
 #endif	/* TEST_H */
--- main.cpp
+++ main.cpp
@@ -13,6 +13,17 @@ using namespace std;
  */
 int main() {
     Board TicTacToe;
+    TicTacToe.setString("x1","1");
+    TicTacToe.setString("x2","2");
+    TicTacToe.setString("x3","3");
+    TicTacToe.setString("x4","4");
+    TicTacToe.setString("x5","5");
+    TicTacToe.setString("x6","6");
+    TicTacToe.setString("x7","7");
+    TicTacToe.setString("x8","8");
+    TicTacToe.setString("x9","9");
+    
+    
     
     while(true){
         cout<<"Welcome! Are you ready? (y/n)";
@@ -20,6 +31,7 @@ int main() {
         getline(cin, input);
         if(input=="y"){
             break;
+            
         }else if(input=="n"){
             cout<<"Come back when you're ready!\n";
         }else{
@@ -27,5 +39,15 @@ int main() {
         }
     }
     
+    cout<<TicTacToe.printBoard();
+    
+    while(true){
+        cout<<"Input a number! ";
+        string input ="";
+        getline(cin, input);
+        
+        break;
+        
+    }
     cout<<TicTacToe.printBoard();
 }   
